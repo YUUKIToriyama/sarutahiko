5d2ad81c964d2f7150381d82751b32c8ec70c2b4
YUUKIToriyama <ytoriyama@torisfactory.com>
2020-01-22 15:56:42 +0900

    async/awaitを用いた書き方に改めました

--- components/js/loadRemoteGeoJSON.js
+++ components/js/loadRemoteGeoJSON.js
@@ -1,4 +1,4 @@
-/* load_remotegeojson.js */
+/* loadRemoteGeoJSON.js */
 
 var loadedRemoteJSONFiles = [];
 var countRemote = 0;
@@ -13,36 +13,30 @@ function getRemoteData(url) {
         })
 }
 
-function loadRemoteGeoJSON() {
+async function loadRemoteGeoJSON() {
 	var textForm = document.getElementById("urlRemoteFile");
 	var url = textForm.value;
 	console.log(url);
-	
-	// FetchAPIを使ってリモートのファイルにアクセス
-	getRemoteData(url).then(data => {
-		var responseURL = data.url;
-		var fileName = responseURL.split("/").pop();
-		var contentType = data.headers.get("Content-Type");
 
-		// オブジェクトへのアクセスはcountRemoteをもちいておこなう
-		data.json().then(geoJson => {
-			loadedRemoteJSONFiles.push(L.geoJSON(geoJson, {
-				// 各フィーチャにポップアップ要素を追加する
-				onEachFeature: function (feature, layer) {
-					var popupText = '<div class="popupText"><table>' + Object.entries(feature).map(x => `<tr><td>${x[0]}</td><td class="value">${JSON.stringify(x[1])}</td></tr>`).join("\n") + '</table></div>';
-					layer.bindPopup(popupText);
-				}	
-			}));
-			// 読み込んだGeoJSONの情報を表に出力する
-			document.getElementById("loadedRemoteGeoJSONs").innerHTML += `<tr><td><input type="checkbox" id="loadedRemoteGeoJSON-${countRemote}" onChange="showRemoteGeoJSON(${countRemote})"/><td>${fileName}</td><td>${responseURL}</td><td>${contentType}</td></tr>`;
-			countRemote = countRemote + 1;
-		}).catch(error => {
-			console.log(error);
-			alert(error);
-		});
-	}).catch(error => {
-		console.log(error)
-	})
+	// FetchAPIを使ってリモートのファイルにアクセス
+	const response = await fetch(url, {method:"GET", redirect:"follow", mode:"cors"}).catch(err => {alert(err); return err});
+	const responseURL = response.url;
+	const fileName = responseURL.split("/").pop();
+	const contentType = response.headers.get("Content-Type");
+	const geoJSON = await response.json().catch(err => {alert(err); return err});
+
+	// 読み込んだGeoJSONにポップアップ要素を付加し、配列loadedRemoteJSONFilesに追加する
+	loadedRemoteJSONFiles.push(L.geoJSON(geoJSON, {
+		onEachFeature: function (feature, layer) {
+			var popupText = '<div class="popupText"><table>' + Object.entries(feature).map(x => `<tr><td>${x[0]}</td><td class="value">${JSON.stringify(x[1])}</td></tr>`).join("\n") + '</table></div>';
+			layer.bindPopup(popupText);
+		}	
+	}));
+
+	// 読み込んだGeoJSONの情報を表に出力する
+	document.getElementById("loadedRemoteGeoJSONs").innerHTML += `<tr><td><input type="checkbox" id="loadedRemoteGeoJSON-${countRemote}" onChange="showRemoteGeoJSON(${countRemote})"/><td>${fileName}</td><td>${responseURL}</td><td>${contentType}</td></tr>`;
+	countRemote = countRemote + 1;
+
 
 	textForm.focus();
 }
